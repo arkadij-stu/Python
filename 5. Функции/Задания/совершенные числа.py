b9abc2ac6b47767a569e5315aeb5b5f7ddf0eb73
arkadij-stu <arkadijlevin018@gmail.com>
2026-02-14 15:30:41 +0300

    решение задач

--- 5. Функции/Задания/совершенные числа.py
+++ 5. Функции/Задания/совершенные числа.py
@@ -1,18 +1,14 @@
-def is_prime(n):
-    if n == 1:
-        return False
-    for i in range(2, n):
+def is_perfect(n):
+    summ = 0
+    for i in range(1, n):
         if n % i == 0:
-            return False
-    return True
-
-
-p = 2
-target_count = int(input())
+            summ += i
+    return summ == n
+n = int(input())
 count = 0
-num = 2^p - 1
-while count != target_count:
-    if is_prime(num):
-        print(2**(p-1)*(2**p-1))
+num = 2
+while count != n:
+    if is_perfect(num):
+        print(num)
         count += 1
-    p += 1
+    num += 1
